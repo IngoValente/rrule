2e6be66d3215da135fe88da5ad68df33d198804c
Ingo Valente <ingo@ingovalente.com>
2024-09-09 00:33:24 +0200

    Again

--- dist/esm/iterinfo/yearinfo.js
+++ dist/esm/iterinfo/yearinfo.js
@@ -1,7 +1,7 @@
 import { __assign } from "tslib";
 import { datetime, getWeekday, isLeapYear, toOrdinal } from '../dateutil.js';
 import { empty, repeat, pymod, includes } from '../helpers.js';
-import { M365MASK, MDAY365MASK, NMDAY365MASK, WDAYMASK, M365RANGE, M366MASK, MDAY366MASK, NMDAY366MASK, M366RANGE, } from '../masks';
+import { M365MASK, MDAY365MASK, NMDAY365MASK, WDAYMASK, M365RANGE, M366MASK, MDAY366MASK, NMDAY366MASK, M366RANGE, } from '../masks.js';
 export function rebuildYear(year, options) {
     var firstyday = datetime(year, 1, 1);
     var yearlen = isLeapYear(year) ? 366 : 365;
